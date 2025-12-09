349c11db602589762b5d33a41bbd066788a4e995
OwnDev <dev.notag@gmail.com>
2025-12-09 19:37:38 +0100

    Update script.js

--- players/script.js
+++ players/script.js
@@ -1,7 +1,7 @@
 fetch("scores.json")
   .then(r => r.json())
   .then(json => {
-    const Table = document.getDocumentById("table-tbody");
+    const Table = document.getDocumentById("table-body");
 
     json.players.forEach(player => {
       const val = document.createElement("tr");
