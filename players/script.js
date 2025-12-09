40dd426efb583e1dc6d1f4772b44ec95055a5c7a
OwnDev <dev.notag@gmail.com>
2025-12-09 18:40:24 +0100

    Create script.js

---
+++ players/script.js
@@ -0,0 +1,15 @@
+fetch("scores.json")
+  .then(r => r.json())
+  .then(json => {
+    const Table = document.querySelector("#Table tbody");
+
+    json.players.forEach(player => {
+      const val = document.createElement("tr");
+      val.innerHTML = `
+        <td>${player.name}</td>
+        <td>${player.id}</td>
+      `;
+
+      Table.appendChild(val);
+    }
+  }
