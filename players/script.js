c080ab9b75609b0f77aa60001ea927c4a718fd5b
OwnDev <dev.notag@gmail.com>
2025-12-10 10:12:53 +0100

    Update script.js

--- players/script.js
+++ players/script.js
@@ -7,7 +7,7 @@ fetch("./scores.json")
       const val = document.createElement("div");
       val.innerHTML = `
       <div class= "player" id= ${player.id}>
-        <img src="../heads/{player.head}"></img>
+        <img src="../heads/{player.head}.png"></img>
         ${player.name}
       </div>`;
 
