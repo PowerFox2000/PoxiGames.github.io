a10d92967de74736c5092d2902a5ec48cc0855d5
OwnDev <dev.notag@gmail.com>
2025-12-11 09:33:03 +0100

    Update script.js

--- players/script.js
+++ players/script.js
@@ -7,7 +7,7 @@ fetch("./scores.json")
       const val = document.createElement("div");
       val.innerHTML = `
       <div class= "player" id= "${player.id}">
-        <img src="${player.head}"></img> <h2><strong>${player.name}</strong></h2>
+        <img src="${player.head}"></img> <h1><strong>${player.name}</strong></h1>
       </div>`;
 
       players.appendChild(val);
