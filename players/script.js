8a0743d8f0dbcbe4b749d83f67bcacf0d39dde11
OwnDev <dev.notag@gmail.com>
2025-12-11 10:40:44 +0100

    Update script.js

--- players/script.js
+++ players/script.js
@@ -6,8 +6,8 @@ fetch("./scores.json")
     json.players.forEach(player => {
       const val = document.createElement("div");
       val.innerHTML = `
-      <div class= "player" id= "${player.minecraft}">
-        <img src="${player.head}"></img> <h1><strong>${player.name}</strong></h1>
+      <div class= "player" id= "${player.id}">
+        <img src="${player.head}"></img> <h1><strong>${player.minecraft}</strong></h1>
       </div>`;
 
       players.appendChild(val);
