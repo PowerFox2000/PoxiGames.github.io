e116b88e014476edc66a000eeab7f6414284a0c8
OwnDev <dev.notag@gmail.com>
2025-12-12 16:02:05 +0100

    Update script.js

--- players/profile/script.js
+++ players/profile/script.js
@@ -1,9 +1,23 @@
 const cx = canvas.width / 2;
 const cy = canvas.height / 2;
-
 const season = 0;
+const count = 8;
 const values = Object.values(player.minigames).map(v => v[season]);
+const out = 100;
 
-const angle = (i / count) * 2 * Math.PI;
-const x = cx + Math.cos(angle) * radius;
-const y = cy + Math.sin(angle) * radius;
+for(let i = 0; i < 2; i++)
+{
+  ctx.beginPath();
+  
+  for(let i = 0; i < count; i++)
+  {
+    const angle = (i / count) * 2 * Math.PI;
+    const x = cx + Math.cos(angle) * radius;
+    const y = cy + Math.sin(angle) * radius;
+    ctx.moveTo(x, y);
+  }
+  
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 3;
+  ctx.stroke();
+}
