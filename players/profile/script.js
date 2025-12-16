f0da7e0c1e01713c81e29366bb049109fb2bd6a3
OwnDev <dev.notag@gmail.com>
2025-12-16 07:32:52 +0100

    Update script.js

--- players/profile/script.js
+++ players/profile/script.js
@@ -1,3 +1,9 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(50, 50, 100, 100);
+
 fetch("../scores.json")
   .then(r => r.json())
   .then(json => {
