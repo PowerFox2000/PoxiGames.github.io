a5650b99f52e9291e4dd8363617635457d821021
theressomeoneelse <theressomebody@gmail.com>
2026-03-12 11:05:51 +0100

    Update script.js

--- poxi/players/script.js
+++ poxi/players/script.js
@@ -1,9 +1,19 @@
+void pointsEver(const player) {
+  let pts = 0;
+  player.points.forEach(pl => {
+    pts += pl;
+  });
+  return pl;
+}
+
 fetch("./data.json")
   .then(r => r.json())
   .then(json => {
     const players = document.getElementById("players");
 
-    json.players.forEach(player => {
+    const playersSorted = players.sort((a, b) => pointsEver(a) - pointsEver(b));
+
+    json.playersSorted.forEach(player => {
       const val = document.createElement("div");
       val.innerHTML = `
       <div class= "player" id= "${player.name}" onclick="go('${player.minecraft}')" style="fontsize: 2.5rem">
