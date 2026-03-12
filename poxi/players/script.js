7322fe24221d647e65f675c05522599f8c541a15
theressomeoneelse <theressomebody@gmail.com>
2026-03-12 19:04:38 +0100

    Update script.js

--- poxi/players/script.js
+++ poxi/players/script.js
@@ -16,7 +16,7 @@ fetch("./data.json")
       const val = document.createElement("div");
 
       val.innerHTML = `
-        <div class="player" id="${player.name}" onclick="go('${player.minecraft}')">
+        <div class="btn-card" id="${player.name}" onclick="go('${player.minecraft}')">
           <img src="https://mc-heads.net/head/${player.minecraft}">
           <h1><strong>${player.minecraft}</strong></h1>
         </div>
