120dd007cf8153fe01608dfee4557e24685c72db
theressomeoneelse <theressomebody@gmail.com>
2026-04-11 18:01:27 +0200

    Update script.js

--- poxi/players/script.js
+++ poxi/players/script.js
@@ -32,29 +32,3 @@ fetch("./data.json")
   .catch(err => console.error(err));function pointsEver(player) {
   return player.points.reduce((sum, p) => sum + p, 0);
 }
-
-fetch("./data.json")
-  .then(r => r.json())
-  .then(json => {
-
-    const players = document.getElementById("players");
-
-    const playersSorted = json.players.sort(
-      (a, b) => pointsEver(b) - pointsEver(a)
-    );
-
-    playersSorted.forEach(player => {
-      const val = document.createElement("div");
-
-      val.innerHTML = `
-        <a class="btn-card" id="${player.name}" href="profile/?id=${encodeURIComponent(player.minecraft)}">
-          <img src="https://mc-heads.net/head/${player.minecraft}">
-          <h1><strong>${player.minecraft}</strong></h1>
-        </a>
-      `;
-
-      players.appendChild(val);
-    });
-
-  })
-  .catch(err => console.error(err));
